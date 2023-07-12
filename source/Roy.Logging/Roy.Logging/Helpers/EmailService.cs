0304622809eea5ec6f237fbb33c2aa68dda8cfdf
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2023-07-11 21:55:59 -0500

    Small changes.

--- source/Roy.Logging/Roy.Logging/Helpers/EmailService.cs
+++ source/Roy.Logging/Roy.Logging/Helpers/EmailService.cs
@@ -1,11 +1,13 @@
-﻿using Roy.Domain.Attributes;
+﻿using Avalon.Base.Extension.Collections;
+using Roy.Domain.Attributes;
 using Roy.Domain.Contants;
 using Roy.Domain.Settings.Web.EmailAspect;
+using System.Net.Mail;
 
 namespace Roy.Logging.Helpers;
 
 /// <summary>
-/// Email helper.
+/// Email service.
 /// </summary>
 public class EmailService
 {
@@ -21,8 +23,22 @@ public class EmailService
     /// <param name="Emails">
     /// Emails to process.
     /// </param>
-    public async void SendAsync(MessageDetail value, Level level, List<EmailSetting> Emails)
+    public async void SendAsync(MessageDetail value, Level level, List<EmailSetting> emails)
     {
+         if(emails.HasElements())
+        {
+            foreach(EmailSetting email in emails)
+            {
+
+            }
+        }
+    }
+
+    public async void SendAsync(MessageDetail value, Level level, EmailSetting email)
+    {
+        //MimeMessage t = new SmtpClient();
+
         var detail = (value is LogDetail) ? value as MessageDetail : value as ExceptionDetail;
+
     }
 }
